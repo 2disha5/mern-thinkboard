51246733839129cd2fbd1db75a3d00bda0b95f55
Disha Chaudhari <dishachaudhari801@gmail.com>
2025-06-17 16:13:13 +0530

    initial commit

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import api from "../lib/axios";
+import { toast } from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
+
+const HomePage = () => {
+  const [isRateLimited, SetIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await api.get("/notes");
+        console.log(res.data);
+        setNotes(res.data);
+      } catch (error) {
+        console.log("Error fetching notes");
+        if (error.response?.status === 429) {
+          SetIsRateLimited(true);
+        } else {
+          toast.error("Error fetching notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
+
+  return (
+    <div className="min-h-screen ">
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
+
+      <div className="max-w7xl max-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10 ">
+            Loading Notes...
+          </div>
+        )}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
