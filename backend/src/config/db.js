09fab3d644935ceaf3549becca00cdda57c9f526
Disha Chaudhari <dishachaudhari801@gmail.com>
2025-06-17 17:19:44 +0530

    trigger redeploy after fixing mongodb whitelist

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
 
     console.log("MongoDB connected successfully");
